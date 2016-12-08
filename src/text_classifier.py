f7bfd65c428fdfbd05ac57392d60e0f35bbd8c6e
AndreCI <andre.cibils@gmail.com>
2016-12-08 16:01:37 +0100

    text_classifier changed, works now

--- src/text_classifier.py
+++ src/text_classifier.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from scipy.sparse import *
+import csv
 import numpy as np
 import pandas as pd
 import sklearn.linear_model as sk
@@ -39,7 +40,8 @@ def construct_features():
     training_set_neg = np.zeros(((np.shape(neg_train)[0],np.shape(embeddings)[1]+1)))
     vocab = open('data/vocab_cut.txt')
     #for each word, search if it is in pos_train or neg_train
-    for word in vocab:
+    for word_ in vocab:
+        word = word_.split("\n")[0]
         current_emb = embeddings[i]
         for j in range(0,np.shape(pos_train)[0]):
             #if yes, add its embeddings.
@@ -56,11 +58,24 @@ def construct_features():
     np.save('data/trainingset_pos', training_set_pos)
     np.save('data/trainingset_neg', training_set_neg)
     
-def predict_labels(data,flag=""):
-    ids = data[:,0]
-    topredict = data[:,1]
-    path_neg = str("data/trainingset_neg_"+flag)
-    path_pos = str("data/trainingset_pos_"+flag)
+def create_csv_submission(ids, y_pred, name):
+    """
+    Creates an output file in csv format for submission to kaggle
+    Arguments: ids (event ids associated with each prediction)
+               y_pred (predicted class labels)
+               name (string name of .csv output file to be created)
+    """
+    with open(name, 'w') as csvfile:
+        fieldnames = ['Id', 'Prediction']
+        writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fieldnames)
+        writer.writeheader()
+        for r1, r2 in zip(ids, y_pred):
+            writer.writerow({'Id':int(r1),'Prediction':int(r2)})
+
+def predict_labels(flag=".npy"):
+    #Load the training set
+    path_neg = str("data/trainingset_neg"+flag)
+    path_pos = str("data/trainingset_pos"+flag)
     ts_neg = np.load(path_neg)
     ts_pos = np.load(path_pos)    
     #Train a Linear Classifier: Train a linear classifier (e.g. logistic regression or SVM) on your constructed 
@@ -69,18 +84,60 @@ def predict_labels(data,flag=""):
     training_set = np.concatenate((ts_neg,ts_pos))
     y = training_set[:,0]
     X = training_set[:,1:np.shape(training_set)[1]]
+    #Construct the logistic regressor
     LR = sk.LogisticRegression()
     #LogisticRegression(penalty='l2', dual=False, tol=0.0001, C=1.0, fit_intercept=True, intercept_scaling=1, 
     #class_weight=None, random_state=None, solver='liblinear', max_iter=100, multi_class='ovr', verbose=0, 
     #warm_start=False, n_jobs=1)[source]¶
     #http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html
+    #train the logistic regressor
     LR.fit(X,y)
+    
+    #Now we load and predict the data
+    data = np.genfromtxt('data/test_data.txt', delimiter="\n",dtype=str)    
+    idx = np.zeros(np.shape(data)[0])
+    tweets = ["" for a in range(0,np.shape(data)[0])]
+    for i in range(0,np.shape(data)[0]):
+        spliter = data[i].split(",")
+        idx[i] = spliter[0]
+        tweet = spliter[1]
+        for j in range(2,np.shape(spliter)[0]):
+            tweet = tweet+","+spliter[j]
+        tweets[i] = tweet
+    #And now, predict the results
+    topredict = construct_features_for_test_set(tweets)
     predictions = LR.predict(topredict)
-    
-    return LR.predict(data)
-    
-    
+    #Construct the submission
+    predictions = predictions*2-1
+    create_csv_submission(idx,predictions,"submission.csv")
 
+def construct_features_for_test_set(test_set_tweet):
+    embeddings = np.load('data/embeddings.npy')
+    word_nbr_per_tweet = np.zeros(np.shape(test_set_tweet)[0])
     
+    for j in range(0,np.shape(test_set_tweet)[0]):
+        tweet = test_set_tweet[j]
+        size = len(re.findall(r'\w+', tweet))
+        if(size==0):
+            size=1
+        word_nbr_per_tweet[j] = size
     
-    
+    vocab = open('data/vocab_cut.txt')
+    test_set = np.zeros(((np.shape(test_set_tweet)[0],np.shape(embeddings)[1])))
+    #for each word, search if it is in a tweet
+    i=0
+    for word_ in vocab:
+        word = word_.split("\n")[0]
+        current_emb = embeddings[i]
+        for j in range(0,50):
+            #if yes, add its embeddings.
+            if word in test_set_tweet[j]:
+                test_set[j,:] += current_emb
+        i+=1
+    #then divide by number of words (averaging word vector over all words of the tweet)
+    for i in range(0,np.shape(embeddings)[1]):
+        test_set[:,i] = test_set[:,i]/word_nbr_per_tweet
+    return test_set
+predict_labels()
+
+
