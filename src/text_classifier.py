a0dfcc80adcddc64bd0a4ad2d41b1a5f3e78baea
maxpre <max.premi@epfl.ch>
2016-12-19 21:04:15 +0100

    Comment And additional stuff in the text_classifier

--- src/text_classifier.py
+++ src/text_classifier.py
@@ -12,61 +12,87 @@ import re
 import string
 
 #This method construct the features of each tweets
-def construct_vectors(data,set_to_fill,vocab,embeddings):
+def construct_vectors(data,set_to_fill,vocab,embeddings) :
+    """
+    Creates an array that contains tweet features representation
+    Arguments: data The set of tweet in string
+               set_to_fill The array you need to fill with the corresponding features representation of the tweet at index i
+               vocab a vocabulary that return the index of the word given in argument when doing vocab.get(word,-1) , and - 1 if not found
+               embedding a Matrix that represent the feature representation of words (in accord of indexes with vocab)
+    """   
     list_auxiliarry_pos = ["must","need","should","may","might","can","could","shall","would","will"]
-    list_auxiliarry_neg = ["won't","shouldn't","not","can't","couldn't","wouldn't"]
+    #used for additional features
+    list_auxiliarry_neg = ["won't","shouldn't","not","can't","couldn't","wouldn't"] #used for additional features
     counter = lambda l1, l2: len(list(filter(lambda c: c in l2, l1))) #Used later to count number fo punctuation
     
-    
+    #for all tweet in the given set "data"
     for j in range(0,np.shape(data)[0]):
-        num_punctu = counter(data[j],string.punctuation)
-        list_word = data[j].split()
-        average = 0
+        num_punctu = counter(data[j],string.punctuation) #count the punctuation
+        list_word = data[j].split() # split in an array of word
+        average = 0 #initialize each additional features
         num3point = 0
         num_aux_pos =0
         num_aux_neg =0
-        for i in list_word:
-            average+=len(i)
+        num_user = 0
+        divider = 0
+        for i in list_word: # for each word
+            average+=len(i) #fill the additional features
             if(i=="..."): 
                 num3point+=1
+            if(i=="<user>"):
+                num_user+=1
             if(i in list_auxiliarry_pos):
                 num_aux_pos+=1
             if(i in list_auxiliarry_neg):
                 num_aux_neg+=1
-            idx = vocab.get(i,-1)
-            if(idx>=0):
+            idx = vocab.get(i,-1) #get the index of the word in the vocabulary (-1 if it does not exist)
+            if(idx>=0): #If the index is found, add the embedding of the word thanks to the index
                 set_to_fill[j,1:np.shape(embeddings)[1]+1] += embeddings[idx]
-        set_to_fill[j,1:np.shape(embeddings)[1]+1] = set_to_fill[j,1:np.shape(embeddings)[1]+1]/len(list_word) #put the average of the word vector in dim [1,21]
+                divider+=1
+        if(divider >0):
+            set_to_fill[j,1:np.shape(embeddings)[1]+1] = set_to_fill[j,1:np.shape(embeddings)[1]+1]/divider #put the average of the word vector in dim [1,21]
         set_to_fill[j,np.shape(embeddings)[1]+1] = len(list_word) #add the # word
         set_to_fill[j,np.shape(embeddings)[1]+2] = num_punctu #add the # punctuation
-        set_to_fill[j,np.shape(embeddings)[1]+3] = average/len(list_word) #add length of word in average
+        if(len(list_word) > 0):
+            set_to_fill[j,np.shape(embeddings)[1]+3] = average/len(list_word) #add length of word in average
+        else :
+            set_to_fill[j,1:np.shape(embeddings)[1]+3] = 0
         set_to_fill[j,np.shape(embeddings)[1]+4] = num_aux_pos #word in a list of auxilarry
         set_to_fill[j,np.shape(embeddings)[1]+5] = num_aux_neg #word in a list of negative aux
-        set_to_fill[j,np.shape(embeddings)[1]+6] = num3point #number of ...
+        set_to_fill[j,np.shape(embeddings)[1]+6] = num3point #number of "..."
+        set_to_fill[j,np.shape(embeddings)[1]+7] = num_user #number of "<user>"
     return set_to_fill
+
+
 def construct_features():
     '''
     construct a feature representation of each training tweet 
     (by averaging the word vectors over all words of the tweet).
     '''
-    #Load the training tweets and the built GloVe word embeddings
-    additional_features = 6
+    #Specify the number of additional features we use (easier to be okay with the size)
+    additional_features = 7
 
+    #Load the two file to train with
     pos_train = open('data/pos_train.txt').readlines()
     neg_train = open('data/neg_train.txt').readlines()
+
+    #Load the feature representation of words
     embeddings = np.load('data/embeddings.npy')
+
+    #Load the vocab (to know wich features correspond to which embeddings)
     with open('data/vocab.pkl', 'rb') as f:
         vocab = pickle.load(f)
-
+    
     pos_mask = np.zeros(np.shape(embeddings)[1]+1+additional_features)
     pos_mask[0] +=1
     #adding 1 at start : this is target (1 is for happy emoji, 0 or -1 for sad face)
-    #will add 3 features , number of word , average length of words, and #punctuation
     training_set_pos = np.zeros(((np.shape(pos_train)[0],np.shape(embeddings)[1]+1+additional_features))) + pos_mask
     training_set_neg = np.zeros(((np.shape(neg_train)[0],np.shape(embeddings)[1]+1+additional_features)))
+    #Create the two set that represent features representation of tweets
     
-    training_set_pos = construct_vectors(pos_train,training_set_pos,vocab,embeddings) #Construct the two training set
+    training_set_pos = construct_vectors(pos_train,training_set_pos,vocab,embeddings) #Filling the two training Set
     training_set_neg = construct_vectors(neg_train,training_set_neg,vocab,embeddings)
+    #Save the training set, to train our model later
     np.save('data/trainingset_pos', training_set_pos)
     np.save('data/trainingset_neg', training_set_neg)
     
@@ -85,6 +111,9 @@ def create_csv_submission(ids, y_pred, name):
             writer.writerow({'Id':int(r1),'Prediction':int(r2)})
 
 def predict_labels(flag=".npy"):
+    """
+    Used to predict the label on a given training Set
+    """
     #Load the training set
     path_neg = str("data/trainingset_neg"+flag)
     path_pos = str("data/trainingset_pos"+flag)
@@ -96,7 +125,6 @@ def predict_labels(flag=".npy"):
     training_set = np.concatenate((ts_neg,ts_pos))
     y = training_set[:,0]
     X = training_set[:,1:np.shape(training_set)[1]]
-    X = build_poly(X,2)
     #Now we load and predict the data
     data = np.genfromtxt('data/test_data.txt', delimiter="\n",dtype=str)    
     idx = np.zeros(np.shape(data)[0])
@@ -116,7 +144,9 @@ def predict_labels(flag=".npy"):
     #warm_start=False, n_jobs=1)[source]¶
     #http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html
     #train the logistic regressor
-    kf = ms.KFold(n_splits=5,shuffle=True)
+
+    #Do a K-fold on the set to have an idea of the error we get
+    kf = ms.KFold(n_splits=3,shuffle=True)
     for train_idx, test_idx in kf.split(X):
         train_set = X[train_idx]
         test_set = X[test_idx]
@@ -128,37 +158,49 @@ def predict_labels(flag=".npy"):
         print(test_target.shape)        
         error = np.sum(np.power(predictions_temp-test_target,2))/np.shape(predictions_temp)[0]
         print("Yet, error is",error)
+    #Fit the model
     LR.fit(X,y)
     
     #And now, predict the results
     topredict = construct_features_for_test_set(tweets)
-    topredict = build_poly(topredict,2)
     predictions = LR.predict(topredict)
     #Construct the submission
     predictions = predictions*2-1
     create_csv_submission(idx,predictions,"submission.csv")
 
 def construct_features_for_test_set(test_set_tweet):
+    """
+    Creates Features representation for the test set, we do not use the same method
+    as the structure is a little different ( no labels)
+               test_set_tweet: the text representation of the given tweets
+    return : the representation in features of the set of tweet
+    """
+    #Load the feature representation of words
     embeddings = np.load('data/embeddings.npy')
+    #Load the vocabulary
     with open('data/vocab.pkl', 'rb') as f:
         vocab = pickle.load(f)
-    additional_features = 6
+    
+    additional_features = 7
     list_auxiliarry_pos = ["must","need","should","may","might","can","could","shall","would","will"]
     list_auxiliarry_neg = ["won't","shouldn't","not","can't","couldn't","wouldn't"]
     test_set = np.zeros((np.shape(test_set_tweet)[0],np.shape(embeddings)[1]+additional_features))
     #Do the same as the training set, but do not have the label at first , so method is a little different
-    for j in range(0,np.shape(test_set)[0]):
-        list_word = test_set_tweet[j].split()
-        divider = 0
+    for j in range(0,np.shape(test_set)[0]): #For all tweet
+        list_word = test_set_tweet[j].split() # Split in an array of word
+        divider = 0 #Initialize the features
         average = 0
         num3point = 0
         num_aux_pos = 0
         num_aux_neg = 0
+        num_user = 0
         counter = lambda l1, l2: len(list(filter(lambda c: c in l2, l1)))
-        num_punctu = counter(test_set_tweet[j],string.punctuation)
-        for i in list_word:
-            idx = vocab.get(i,-1)
-            average+=len(i)
+        num_punctu = counter(test_set_tweet[j],string.punctuation) # Count number of punctuation
+        for i in list_word: # For each word in the tweet
+            idx = vocab.get(i,-1)  #Search for the index of the word
+            average+=len(i) # Fill the different features
+            if(i=="<user>"):
+                num_user +=1
             if(i=="..."):
                 num3point+=1
             if(i in list_auxiliarry_pos):
@@ -167,7 +209,7 @@ def construct_features_for_test_set(test_set_tweet):
                 num_aux_neg+=1
             if(idx>=0):
                 divider+=1
-                test_set[j,:np.shape(embeddings)[1]] += embeddings[idx]
+                test_set[j,:np.shape(embeddings)[1]] += embeddings[idx] # Add the embeddings of the word if found
         if(divider >0):
             test_set[j,:np.shape(embeddings)[1]] = test_set[j,:np.shape(embeddings)[1]]/divider
         test_set[j,np.shape(embeddings)[1]] = len(list_word) #add the # word
@@ -179,7 +221,9 @@ def construct_features_for_test_set(test_set_tweet):
         test_set[j,np.shape(embeddings)[1]+3] = num_aux_pos
         test_set[j,np.shape(embeddings)[1]+4] = num_aux_neg
         test_set[j,np.shape(embeddings)[1]+5] = num3point
-    #then divide by number of words (averaging word vector over all words of the tweet)
+        test_set[j,np.shape(embeddings)[1]+6] = num_user
     return test_set
-construct_features()
-predict_labels()
+
+#To run the code do
+#construct_features()
+#predict_labels()
