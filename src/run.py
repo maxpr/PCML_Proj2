883d1a69d7a924bc2e877e90c64cc11ce0d187cf
Drnoodle <hugo.bonnome@epfl.ch>
2016-12-22 14:42:42 +0100

    add cleaning data function in run.py

--- src/run.py
+++ src/run.py
@@ -1,4 +1,22 @@
-from word2vec_routines import *
+
+from src.dataCleaning import dataCleaning
+from src.word2vec_routines import construct_features, predict_labels
+
+
+
+def cleanTrainingSet(pathToOrigFile, pathToOutFile):
+    dataC = dataCleaning(pathToOrigFile)
+    dataC.setDuplicateLinesRemoved()
+    dataC.setTweetTransform()
+    dataC.save(pathToOutFile)
+
+def cleanTest(pathToOrigFile, pathToOutFile):
+    dataC = dataCleaning(pathToOrigFile)
+    dataC.setRemoveTestTweetId()
+    dataC.setTweetTransform()
+    dataC.save(pathToOutFile)
+
+
 
 path_train = "data//full_train_clean_v1.txt"
 path_pos = "data/pos_train_clean_v1.txt"
@@ -6,3 +24,5 @@ path_neg = "data/neg_train_clean_v1.txt"
 path_testing = "data/test_clean_v1.txt"
 model =  construct_features(path_pos,path_neg,path_train)
 predict_labels(model,path_testing)
+
+
