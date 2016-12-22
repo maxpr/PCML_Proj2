96151bb0ce28d2622838ee53966f2367ac77063e
Drnoodle <hugo.bonnome@epfl.ch>
2016-12-22 15:15:18 +0100

    add argument interface

--- src/run.py
+++ src/run.py
@@ -1,6 +1,8 @@
+import sys
+
 from src.boosting.adaboost import adaboost
 from src.dataCleaning import dataCleaning
-from src.word2vec_routines import construct_features, predict_labels
+"""from src.word2vec_routines import construct_features, predict_labels"""
 
 
 
@@ -17,12 +19,14 @@ def cleanTest(pathToOrigFile, pathToOutFile):
     dataC.save(pathToOutFile)
 
 
-def runBoosting(negTrain, posTrain, negTest, posTest):
+def runBoosting(negTrain, posTrain, negTest, posTest, kaggleTest):
 
     # 1. Open the training data :
 
     pos = dataCleaning(posTrain).getData()
     neg = dataCleaning(negTrain).getData()
+
+    kaggleTest = dataCleaning(kaggleTest).getData()
 
     posTest = dataCleaning(posTest).getData()
     negTest = dataCleaning(negTest).getData()
@@ -33,15 +37,25 @@ def runBoosting(negTrain, posTrain, negTest, posTest):
 
     # 3. Start the algorithm :
 
-    boosting.run('result.txt', posTest, negTest)
+    boosting.run('result.txt', posTest, negTest, kaggleTest)
 
 
+if len(sys.argv) != 2:
+    print("you have to specify one of the following entry : ")
+elif sys.argv[1] == "boosting":
+    runBoosting("data/neg_train.txt",
+                "data/pos_train.txt",
+                "data/neg_train.txt",
+                "data/pos_train.txt",
+                "data/test_data.txt",)
 
-path_train = "data//full_train_clean_v1.txt"
-path_pos = "data/pos_train_clean_v1.txt"
-path_neg = "data/neg_train_clean_v1.txt"
-path_testing = "data/test_clean_v1.txt"
-model =  construct_features(path_pos,path_neg,path_train)
-predict_labels(model,path_testing)
+
+if False:
+    path_train = "data//full_train_clean_v1.txt"
+    path_pos = "data/pos_train_clean_v1.txt"
+    path_neg = "data/neg_train_clean_v1.txt"
+    path_testing = "data/test_clean_v1.txt"
+    model =  construct_features(path_pos,path_neg,path_train)
+    predict_labels(model,path_testing)
 
 
